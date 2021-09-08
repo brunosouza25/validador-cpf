547356d619b940655111155f79ba03fb581fa9f4
brunosouza25 <brunopw25@gmail.com>
2021-09-08 19:33:52 -0300

    alterado a função de validar cpf para o prototype do objeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,60 +31,60 @@ function Cpf() {
             cpf = cpfValidate;
         },
     });
-
-    /**
-     * Função responsável por validar CPF
-     * @param {string} string cpf a ser válidado
-     * @returns {*} retorna false se o cpf não for válido ou o própio se for válido
-     */
-    this.validateCpf = function (string) {
-        const rawCpf = string.replace(/\D+/g, '');
-
-        if (isNaN(rawCpf) || rawCpf.length !== 11) {
-            return false;
-        }
-
-        let equals = false;
-        //Validar se o cpf é inválido do tipo sequencial, ex: 11111111111, 55555555555, 99999999999
-        for (let i = 0; i < rawCpf.length - 1; i++) {
-            if (rawCpf[i] == rawCpf[i + 1]) {
-                equals = true;
-            } else {
-                equals = false;
-                break;
-            }
-        }
-        if (equals) {
-            writeResultCpf(!equals);
-            return;
-        }
-
-        let sum = 0;
-
-        for (let i = 10; i >= 2; i--) {
-            sum += rawCpf[10 - i] * i;
-        }
-
-        const verifyOne = (sum * 10) % 11 === 10 ? 0 : (sum * 10) % 11;
-
-        const secondRawCpf = String(rawCpf.slice(0, 9)) + String(verifyOne);
-
-        sum = 0;
-
-        for (let i = 11; i >= 2; i--) {
-            sum += secondRawCpf[11 - i] * i;
-        }
-
-        const verifyTwo = (sum * 10) % 11 === 10 ? 0 : (sum * 10) % 11;
-
-        const newCpf = String(secondRawCpf) + String(verifyTwo);
-        if (rawCpf !== newCpf) {
-            return false;
-        }
-
-        return newCpf;
-    };
 }
+
+/**
+ * Função responsável por validar CPF
+ * @param {string} string cpf a ser válidado
+ * @returns {*} retorna false se o cpf não for válido ou o própio se for válido
+ */
+Cpf.prototype.validateCpf = function (string) {
+    const rawCpf = string.replace(/\D+/g, '');
+
+    if (isNaN(rawCpf) || rawCpf.length !== 11) {
+        return false;
+    }
+
+    let equals = false;
+    //Validar se o cpf é inválido do tipo sequencial, ex: 11111111111, 55555555555, 99999999999
+    for (let i = 0; i < rawCpf.length - 1; i++) {
+        if (rawCpf[i] == rawCpf[i + 1]) {
+            equals = true;
+        } else {
+            equals = false;
+            break;
+        }
+    }
+    if (equals) {
+        writeResultCpf(!equals);
+        return;
+    }
+
+    let sum = 0;
+
+    for (let i = 10; i >= 2; i--) {
+        sum += rawCpf[10 - i] * i;
+    }
+
+    const verifyOne = (sum * 10) % 11 === 10 ? 0 : (sum * 10) % 11;
+
+    const secondRawCpf = String(rawCpf.slice(0, 9)) + String(verifyOne);
+
+    sum = 0;
+
+    for (let i = 11; i >= 2; i--) {
+        sum += secondRawCpf[11 - i] * i;
+    }
+
+    const verifyTwo = (sum * 10) % 11 === 10 ? 0 : (sum * 10) % 11;
+
+    const newCpf = String(secondRawCpf) + String(verifyTwo);
+    if (rawCpf !== newCpf) {
+        return false;
+    }
+
+    return newCpf;
+};
 
 //Função responsável por exibir na tela o resultado da validação
 function writeResultCpf(cpfValidate) {
