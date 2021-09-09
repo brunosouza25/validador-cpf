8bddc6e6f3ca2c3c4c479806299581595bd19f6f
brunosouza25 <brunopw25@gmail.com>
2021-09-08 21:21:03 -0300

    Necessita verificar o porque a Setter do CPF não consegue chamar a função verificationNumbers do prototype

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,69 +22,39 @@ function Cpf() {
         get: function () {
             return cpf;
         },
-        set: function (string) {
-            const cpfValidate = validateCpf(string);
-            if (!cpfValidate) {
-                return false;
-            }
-
-            cpf = cpfValidate;
-        },
     });
 }
 
-/**
- * Função responsável por validar CPF
- * @param {string} string cpf a ser válidado
- * @returns {*} retorna false se o cpf não for válido ou o própio se for válido
- */
-Cpf.prototype.validateCpf = function (string) {
-    const rawCpf = string.replace(/\D+/g, '');
+//TODO
+//Necessita verificar o porque a Setter do CPF não consegue chamar a função "verificationNumbers" do prototype
+Cpf.prototype.verificationNumbers = function (string) {
+    let rawCpf = String(string).split('');
+    let count = 10;
+    let total = rawCpf.reduce((sum, item) => {
+        sum += count * item;
+        count--;
+        return sum;
+    }, 0);
 
-    if (isNaN(rawCpf) || rawCpf.length !== 11) {
-        return false;
-    }
+    let digit = 11 - (total % 11) > 9 ? 0 : 11 - (total % 11);
 
-    let equals = false;
-    //Validar se o cpf é inválido do tipo sequencial, ex: 11111111111, 55555555555, 99999999999
-    for (let i = 0; i < rawCpf.length - 1; i++) {
-        if (rawCpf[i] == rawCpf[i + 1]) {
-            equals = true;
-        } else {
-            equals = false;
-            break;
-        }
-    }
-    if (equals) {
-        writeResultCpf(!equals);
-        return;
-    }
+    rawCpf.push(digit);
 
-    let sum = 0;
+    count = 11;
 
-    for (let i = 10; i >= 2; i--) {
-        sum += rawCpf[10 - i] * i;
-    }
+    total = rawCpf.reduce((sum, item) => {
+        sum += item * count;
+        count--;
+        return sum;
+    }, 0);
 
-    const verifyOne = (sum * 10) % 11 === 10 ? 0 : (sum * 10) % 11;
-
-    const secondRawCpf = String(rawCpf.slice(0, 9)) + String(verifyOne);
-
-    sum = 0;
-
-    for (let i = 11; i >= 2; i--) {
-        sum += secondRawCpf[11 - i] * i;
-    }
-
-    const verifyTwo = (sum * 10) % 11 === 10 ? 0 : (sum * 10) % 11;
-
-    const newCpf = String(secondRawCpf) + String(verifyTwo);
-    if (rawCpf !== newCpf) {
-        return false;
-    }
-
-    return newCpf;
+    digit = 11 - (total % 11) > 9 ? 0 : 11 - (total % 11);
+    rawCpf.push(digit);
+    return rawCpf.join('');
 };
+
+//70548445052
+//07098772003
 
 //Função responsável por exibir na tela o resultado da validação
 function writeResultCpf(cpfValidate) {
