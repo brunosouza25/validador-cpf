7933b6d54ec1c1fab0847d4bafc9be6e542f765a
brunosouza25 <brunopw25@gmail.com>
2021-09-13 21:26:27 -0300

    refatorado o objeto do CPF

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,74 @@
-const cpf = new Cpf();
+class Cpf {
+    static cpfGenerator() {
+        let newCpf;
+        //while para verificar se o cpg gerado não é uma sequência
+        while (isNaN(newCpf)) {
+            let rawCpf = [];
+            for (let i = 0; i <= 9; i++) {
+                rawCpf.push(Math.floor(Math.random() * 10));
+            }
+
+            newCpf = this.verificationNumbers(rawCpf.join(''));
+        }
+        return newCpf;
+    }
+
+    static verificationNumbers(string) {
+        let rawCpf = String(string).slice(0, 9).split('');
+        const itsSequence = this.itsASequence(rawCpf.join(''));
+
+        if (itsSequence) {
+            writeResultCpf(false);
+            return;
+        }
+
+        let count = 10;
+
+        let total = rawCpf.reduce((sum, item) => {
+            sum += count * item;
+            count--;
+            return sum;
+        }, 0);
+
+        let digit = 11 - (total % 11) > 9 ? 0 : 11 - (total % 11);
+
+        rawCpf.push(digit);
+
+        count = 11;
+
+        total = rawCpf.reduce((sum, item) => {
+            sum += item * count;
+            count--;
+            return sum;
+        }, 0);
+
+        digit = 11 - (total % 11) > 9 ? 0 : 11 - (total % 11);
+        rawCpf.push(digit);
+
+        return rawCpf.join('');
+    }
+
+    //verificar se o cpf é uma sequencia de numeros ex: 11111111111, 22222222222...
+    static itsASequence(string) {
+        return String(string).charAt(0).repeat(string.length) == string;
+    }
+}
+
 const result = document.getElementById('result');
 const input = document.getElementById('cpf');
 
 document.getElementById('generate').addEventListener('click', () => {
     input.value = '';
-    input.value = cpf.cpfGenerator();
+    input.value = Cpf.cpfGenerator();
 });
 
-//TODO
-//FAZER A COPIA DO CPF
 document.getElementById('copy').addEventListener('click', () => {
     navigator.clipboard.writeText(input.value);
 });
 
 document.getElementById('validate').addEventListener('click', () => {
-    const result = cpf.verificationNumbers(
-        document.getElementById('cpf').value
-    );
     writeResultCpf(
-        cpf.verificationNumbers(document.getElementById('cpf').value) ==
+        Cpf.verificationNumbers(document.getElementById('cpf').value) ==
             document.getElementById('cpf').value
     );
 });
@@ -28,74 +78,9 @@ document.getElementById('cpf').addEventListener('keypress', (event) => {
         return;
     }
     writeResultCpf(
-        cpf.verificationNumbers(document.getElementById('cpf').value)
+        Cpf.verificationNumbers(document.getElementById('cpf').value)
     );
 });
-
-//Objeto CPF
-function Cpf() {
-    let cpf;
-
-    Object.defineProperty(this, 'cpf', {
-        enumerable: true,
-        configurable: true,
-        get: function () {
-            return cpf;
-        },
-        set: function (string) {
-            const cpfValidate = this.verificationNumbers(string);
-
-            if (cpfValidate != string) {
-                console.log(cpfValidate);
-
-                return false;
-            }
-            console.log(cpfValidate);
-        },
-    });
-}
-
-Cpf.prototype.verificationNumbers = function (string) {
-    let rawCpf = String(string).slice(0, 9).split('');
-    let count = 10;
-    let total = rawCpf.reduce((sum, item) => {
-        sum += count * item;
-        count--;
-        return sum;
-    }, 0);
-
-    let digit = 11 - (total % 11) > 9 ? 0 : 11 - (total % 11);
-
-    rawCpf.push(digit);
-
-    count = 11;
-
-    total = rawCpf.reduce((sum, item) => {
-        sum += item * count;
-        count--;
-        return sum;
-    }, 0);
-
-    digit = 11 - (total % 11) > 9 ? 0 : 11 - (total % 11);
-    rawCpf.push(digit);
-
-    return rawCpf.join('');
-};
-
-Cpf.prototype.cpfGenerator = function () {
-    let rawCpf = [];
-
-    for (let i = 0; i <= 9; i++) {
-        rawCpf.push(Math.floor(Math.random() * 10));
-    }
-
-    const newCpf = this.verificationNumbers(rawCpf.join(''));
-
-    return newCpf;
-};
-
-//70548445052
-//07098772003
 
 //Função responsável por exibir na tela o resultado da validação
 function writeResultCpf(cpfValidate) {
